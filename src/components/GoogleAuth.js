645d8acb7b592de2d7b575a0c875722b8354bb44
Thanat <bztzn512@gmail.com>
2021-10-11 19:34:11 -0400

    auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 
 export class GoogleAuth extends Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "248239315370-7badboorgao7c2f7ju6dd9e5s8vlmtlt.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "248239315370-7badboorgao7c2f7ju6dd9e5s8vlmtlt.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div></div>;
+    }
+  }
   render() {
     return <div></div>;
   }
