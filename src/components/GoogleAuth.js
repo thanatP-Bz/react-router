6524c8ada4b4e3687a215c7f0d470cec4e00f834
Thanat <bztzn512@gmail.com>
2021-10-12 21:01:52 -0400

    googleAuth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -13,17 +13,26 @@ export class GoogleAuth extends Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.auth.onAuthChange);
         });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return <div></div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
     }
   }
   render() {
-    return <div></div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
