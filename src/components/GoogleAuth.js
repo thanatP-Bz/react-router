9d34aa4cbf3ddb51769fce0cdf8a0b1c3ce31c43
Thanat <bztzn512@gmail.com>
2021-10-13 15:17:32 -0400

    auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export class GoogleAuth extends Component {
   state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
       window.gapi.client
@@ -22,13 +23,30 @@ export class GoogleAuth extends Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div></div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>;
+      return (
+        <button onClick={this.onSignOut} className="ui red button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign In With Google
+        </button>
+      );
     }
   }
   render() {
