27944c174aec780d714cc11361d7f976b99f43cd
Thanat <bztzn512@gmail.com>
2021-10-16 19:29:37 -0400

    reduxWithAuth

---
+++ src/Action/index.js
@@ -0,0 +1,13 @@
+import { SIGN_OUT, SIGN_IN } from "./type";
+
+export const signIn = (userId) => {
+  return {
+    type: SIGN_IN,
+    payload: userId,
+  };
+};
+export const signOut = () => {
+  return {
+    type: SIGN_OUT,
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import Reducer from "./Reducer";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(Reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
