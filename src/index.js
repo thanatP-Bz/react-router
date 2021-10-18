08b33158bb2722fe40c8fadb8dd160876f104d5e
Thanat <bztzn512@gmail.com>
2021-10-17 22:48:07 -0400

    reduxDevTools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import "./index.css";
 import Reducer from "./Reducer";
 import App from "./App";
 
-const store = createStore(Reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(Reducer, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
